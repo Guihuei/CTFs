95b231a4b24e4ed1f9a8c81a2204f82251651690
Guihuei <g.braga2003@icloud.com>
2024-11-18 15:51:12 -0500

    Automated commit message

--- gitpush.py
+++ gitpush.py
@@ -4,7 +4,7 @@ import os
 # Defina o repositório e o token de autenticação
 repo_path = "/home/kali/Desktop/CTFs/"  # Caminho local do seu repositório
 github_token = "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"
-repo_url = f"https://{github_token}@github.com/usuario/repo.git"  # Substitua 'usuario/repo' pelo seu repositório
+repo_url = f"https://{github_token}@github.com/Guihuei/CTFs"  # Substitua 'usuario/repo' pelo seu repositório
 
 # Defina o diretório do repositório
 os.chdir(repo_path)
